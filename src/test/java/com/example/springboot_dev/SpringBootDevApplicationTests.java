f2e70bf75d299be9f14f57ccc0e8efe214894775
kyj91032 <sees77770@gmail.com>
2023-07-01 17:54:07 +0900

    Initial commit

---
+++ src/test/java/com/example/springboot_dev/SpringBootDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
