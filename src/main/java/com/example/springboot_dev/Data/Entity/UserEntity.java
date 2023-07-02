62691bbc38162a9986c0ba169f2f291011940b9e
kyj91032 <sees77770@gmail.com>
2023-07-02 21:42:58 +0900

    yml, gradle 재설정. userEntity 추가

--- src/main/java/com/example/springboot_dev/Data/Entity/UserEntity.java
+++ src/main/java/com/example/springboot_dev/Data/Entity/UserEntity.java
@@ -1,8 +1,24 @@
 package com.example.springboot_dev.Data.Entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+
 @Getter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
 public class UserEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId")
+    private Long id;
+
+    @Column(name = "userName", length = 10, nullable = false)
+    private String userName;
+
+    @Column(name = "password", length = 20, nullable = false)
+    private String pw;
 }
--- src/main/java/com/example/springboot_dev/SpringBootDevApplication.java
+++ src/main/java/com/example/springboot_dev/SpringBootDevApplication.java
@@ -8,6 +8,7 @@ public class SpringBootDevApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDevApplication.class, args);
+
 	}
 
 }
